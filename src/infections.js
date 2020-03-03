15a46cb2c1bbcf225368de5e20705987ac6085ef
Dominik Voss <vossdominik@arcor.de>
2020-03-04 00:11:52 +0100

    changed source of data to bno spreadsheet

--- src/infections.js
+++ src/infections.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 
-const SOURCE_URL = 'https://bnonews.com/index.php/2020/02/the-latest-coronavirus-cases/';
+const SOURCE_URL = 'https://docs.google.com/spreadsheets/u/0/d/e/2PACX-1vR30F8lYP3jG7YOq8es0PBpJIE5yvRVZffOyaqC0GgMBN6yt0Q-NI8pxS7hd1F9dYXnowSC6zpZmW9D/pubhtml/sheet?headers=false&gid=0';
 
 const getAllInfections = async (countries) => {
   const response = await fetch(SOURCE_URL);
@@ -10,7 +10,7 @@ const getAllInfections = async (countries) => {
   const allData = {};
 
   countries.forEach((country) => {
-    const dataRow = $(`table.wp-block-table.is-style-regular tr:contains("${country}") td`);
+    const dataRow = $(`table.waffle tr:contains("${country}") td`);
     const data = {
       country: dataRow[0].children[0].data,
       infections: parseInt(dataRow[1].children[0].data.replace(',', ''), 10),
